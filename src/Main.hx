b2c3d7857423f2cf077a43ff512cae0786b87be8
Ishibashi Hideto <hideto@zerobase.jp>
2013-08-09 22:06:52 +0900

    (trivial change: fix indent)

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,7 @@
 class Main {
     static function main() {
 		var bar = new ConcreteBar();
-        var foo = new Foo(bar);
+		var foo = new Foo(bar);
 		foo.bar_set_str("Hello World !");
 		trace(foo.bar_get_str());
     }
