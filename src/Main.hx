9deca590127db14b2f2419a25521ebc1d59c2bf5
Ishibashi Hideto <hideto@zerobase.jp>
2013-08-09 22:15:14 +0900

    (reindent Main.hx)

--- src/Main.hx
+++ src/Main.hx
@@ -1,8 +1,8 @@
 class Main {
-    static function main() {
+	static function main() {
 		var bar = new ConcreteBar();
 		var foo = new Foo(bar);
 		foo.bar_set_str("Hello World !");
 		trace(foo.bar_get_str());
-    }
+	}
 }
